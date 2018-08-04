51f19694ee75cb327dbff5fa44d9f79ba0efb1ea
Anurag Sharma <anssharma61@gmail.com>
2018-08-04 10:30:22 +0530

    [general] Minor fixes for extending django-ipam

--- django_ipam/models.py
+++ django_ipam/models.py
@@ -7,12 +7,14 @@ class Subnet(AbstractSubnet):
     class Meta(AbstractSubnet.Meta):
         abstract = False
         swappable = swappable_setting('django_ipam', 'Subnet')
+        app_label = 'django_ipam'
 
 
 class IpAddress(AbstractIpAddress):
     class Meta(AbstractIpAddress.Meta):
         abstract = False
         swappable = swappable_setting('django_ipam', 'IpAddress')
+        app_label = 'django_ipam'
 
 
 class CsvImportException(Exception):
--- django_ipam/tests/test_commands.py
+++ django_ipam/tests/test_commands.py
@@ -5,5 +5,6 @@ from .base.test_commands import BaseTestCommands
 
 
 class TestCommands(BaseTestCommands, TestCase):
+    app_name = 'django_ipam'
     subnet_model = swapper.load_model('django_ipam', 'Subnet')
     ipaddress_model = swapper.load_model('django_ipam', 'IpAddress')
